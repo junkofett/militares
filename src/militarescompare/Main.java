30a09ad891918dbeded40cbe98f18bc02695269c
junkofett <leonardomo84@gmail.com>
2014-05-17 00:12:16 +0200

    primer

--- src/militarescompare/Main.java
+++ src/militarescompare/Main.java
@@ -29,7 +29,7 @@ public class Main {
        
               System.out.println("lo quiero todo");
        
-        System.out.println("TODO!!");
+        System.out.println("TODO!! ");
        
         System.out.println("");
 
