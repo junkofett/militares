1ec7f4a3f1a9443bdc40211b49510eddcaef8abd
vladiperezrodriguez <Lucky@Lucky-HP>
2014-05-16 19:03:44 +0200

    iraaaaa

--- src/militarescompare/Main.java
+++ src/militarescompare/Main.java
@@ -20,6 +20,6 @@ public class Main {
        c.mostrarTropa();
        c.ordenarSort();
        c.mostrarTropa();
-       System.out.println("hola");
+       System.out.println("hola hola");
     }
 }
